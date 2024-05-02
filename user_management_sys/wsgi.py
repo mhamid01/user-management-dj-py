e5638f589b5919b9ba2c8d6e650bfb9d209c77f0
hamid <mhamid6611@gmail.com>
2024-05-02 14:08:27 +0500

    Project Name corrections

--- user_management_sys/wsgi.py
+++ user_management_sys/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for user_management_sys1 project.
+WSGI config for user_management_sys project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "user_management_sys1.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "user_management_sys.settings")
 
 application = get_wsgi_application()
