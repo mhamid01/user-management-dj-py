cad5a009fcfb95bde622bd62b50105f46c5d2b85
hamid <mhamid6611@gmail.com>
2024-05-06 12:21:41 +0500

    Applying Try catch

--- user_control/auth/LoginView.py
+++ user_control/auth/LoginView.py
@@ -9,16 +9,19 @@ from user_control.models import User
 class LoginView(APIView):
     @staticmethod
     def post(request):
-        if request.method == 'POST':
-            email = request.data.get('email')
-            password = request.data.get('password')
-            user = User.objects.filter(email=email).first()
-            if user:
-                if user.check_password(password):
-                    # Generate JWT token
-                    refresh = RefreshToken.for_user(user)
-                    return Response({'token': str(refresh.access_token)}, status=status.HTTP_200_OK)
+        try:
+            if request.method == 'POST':
+                email = request.data.get('email')
+                password = request.data.get('password')
+                user = User.objects.filter(email=email).first()
+                if user:
+                    if user.check_password(password):
+                        # Generate JWT token
+                        refresh = RefreshToken.for_user(user)
+                        return Response({'token': str(refresh.access_token)}, status=status.HTTP_200_OK)
+                    else:
+                        return Response({'error': 'Invalid password'}, status=status.HTTP_401_UNAUTHORIZED)
                 else:
-                    return Response({'error': 'Invalid password'}, status=status.HTTP_401_UNAUTHORIZED)
-            else:
-                return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+                    return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- user_control/auth/RegistrationView.py
+++ user_control/auth/RegistrationView.py
@@ -9,12 +9,16 @@ from user_control.serializers import CustomUserSerializer
 class RegistrationView(APIView):
     @staticmethod
     def post(request):
-        if request.method == 'POST':
-            serializer = CustomUserSerializer(data=request.data)
-            if serializer.is_valid():
-                user = serializer.save()
-                # Generate JWT token
-                refresh = RefreshToken.for_user(user)
-                # refresh_token = str(refresh)
-                return Response({'token': str(refresh.access_token)}, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            if request.method == 'POST':
+                serializer = CustomUserSerializer(data=request.data)
+
+                if serializer.is_valid():
+                    user = serializer.save()
+                    # Generate JWT token
+                    refresh = RefreshToken.for_user(user)
+                    return Response({'token': str(refresh.access_token)}, status=status.HTTP_201_CREATED)
+
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
